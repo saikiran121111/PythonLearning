e9a9511c00e23dec58da578a3c920bf4158031f0
Saikiran <saikiranvsk3@gmail.com>
2026-03-25 14:20:19 +0530

    Updated Python

--- Challenges/arcade.py
+++ Challenges/arcade.py
@@ -1,26 +1,33 @@
+import sys
+
 import rps
 import guess_number
 
 def game_select(name):
+    global play
+    print('Welcome to the Arcade 🫡🫡🫡')
     try:
-        user_selection = int(input(f'''{name} choose a game:\n1 = Rock Paper Scissors\n2 = Guess My number\n'''))
+        user_selection = input(f'''{name} choose a game:\n1 = Rock Paper Scissors\n2 = Guess My number\n\nOr press "x" to exit the Arcade\n''')
 
-        user_validation = (1,2)
+        user_validation = ('1','2')
 
         if user_selection not in user_validation:
-            print(f'Please enter a valid option, {name}')
-            return game_select(name)
+            if user_selection.lower() == 'x':
+                print(f'Thanks for playing!, {name}')
+                sys.exit()
+            else:
+                print(f'Please enter a valid option, {name}')
+                return game_select(name)
         elif user_selection == user_validation[0]:
             play = rps.rps_game(name)
         else:
             play = guess_number.guess(name)
-
         play()
+        return game_select(name)
 
     except ValueError:
         print(f'Please enter a valid number,{name}')
         game_select(name)
-
 
 if __name__ == '__main__':
     import argparse
